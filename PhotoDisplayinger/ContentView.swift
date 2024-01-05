30531550d033a982874a95f9bc06b8cfc45d0e2e
Maria Zaitseva <maria@tail-call.ru>
2024-01-05 14:25:39 +0700

    Initial Commit

---
+++ PhotoDisplayinger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoDisplayinger
+//
+//  Created by Maria Zaitseva on 05.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoDisplayinger/PhotoDisplayingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDisplayingerApp.swift
+//  PhotoDisplayinger
+//
+//  Created by Maria Zaitseva on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDisplayingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
